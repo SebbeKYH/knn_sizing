2b7c9d429eb1200dcdd59d96628164210fa409e5
SebbeKYH <sebastian.andersson2@student.kyh.se>
2022-04-08 11:38:09 +0200

    to be continued...

--- actuall_progress.py
+++ actuall_progress.py
@@ -2,24 +2,25 @@ import csv
 from _csv import reader
 import math
 
-def get_weight():
-    weight = []
-    with open(r'df_male_weight.csv') as csvfile:
+def get_data_frame():
+    data_frame = []
+    with open(r'df_male_round.csv') as csvfile:
         lines = csv.reader(csvfile)
         for row in lines:
-            weight.append(row[1])
-    return weight
+            data_frame.append(row)
+    return data_frame
 
-def get_height():
-    height = []
-    with open (r'df_male_height.csv') as csvfile:
-        lines = csv.reader(csvfile)
-        for row in lines:
-            height.append(row[1])
-    return height
+def get_sizes(data_frame):
+    sizes = []
+    for size in data_frame['chest_height_mm']:
+        if data_frame <= 736:
+            sizes.append('Small')
+
 
 
 def main():
+    data_frame = get_data_frame()
+    print(get_sizes(data_frame))
 
 
 if __name__ == "__main__":
