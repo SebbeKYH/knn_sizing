4f07798b37514fdc9625d71e312eb8725d545760
SebbeKYH <sebastian.andersson2@student.kyh.se>
2022-04-13 18:12:49 +0200

    done

--- get_sizes.py
+++ get_sizes.py
@@ -1,46 +1,51 @@
 import pandas as pd
-import math
 from math import sqrt
-from scipy.spatial import distance
+
 
 
 def get_data_frame():
+    # Display all rows
     pd.set_option('display.max_rows', None)
+    # Display all columns
     pd.set_option('display.max_columns', None)
+    # Read the csv file to dataframe
     data_frame = pd.read_csv('./df_male_last_one.csv')
     return data_frame
 
 
 def get_sizes(data_frame):
+    # Store the sizes based on measurments from dataframe
     size = []
     for height, weight in zip (data_frame['height_cm'], data_frame['weight_hg']):
-        if 170 <= height < 178 & weight <= 550 < 650:
-            size.append('small')
-        elif 174 >= height < 182 & weight <= 650 < 750:
-            size.append('medium')
-        elif 1076 >= height < 186 & weight <= 750 < 850:
-            size.append('large')
-        elif 178 <= height < 190 & weight <= 850 < 900:
-            size.append('x-large')
-        elif 180 <= height < 194 & weight <= 900 < 950:
-            size.append('xx-large')
-        elif height > 196 & weight > 1000:
-            size.append('xxx-large')
-        elif height < 169 & weight < 550:
+        if (height <= 169) & (weight <= 550):
             size.append('x-small')
+        elif (height <= 178) & (weight <= 650):
+            size.append('small')
+        elif (height <= 182) & (weight <= 750):
+            size.append('medium')
+        elif (height <= 186) & (weight <= 850):
+            size.append('large')
+        elif (height <= 190) & (weight <= 900):
+            size.append('x-large')
+        elif (height <= 194) & (weight <= 950):
+            size.append('xx-large')
+        else:
+            size.append('xxx-large')
     return size
 
 
-def calculate_distance(user_data, X_train):
+def calculate_distance(test_row, train_row):
+    # calculate the distance between points in user and dataframe
     distance = 0.0
-    for i in range(len(user_data)-1):
-        distance = (user_data[i] - X_train[i]) ** 2
+    for i in range(len(test_row)):
+        distance += (test_row[i] - train_row[i]) ** 2
     return sqrt(distance)
 
-def get_neighbors(X_train, user_data, k_value):
-    distances = []
+
+def get_neighbors(X_train, test_row, k_value):
+    distances = list()
     for train_row in X_train:
-        dist = calculate_distance(user_data, train_row)
+        dist = calculate_distance(test_row, train_row)
         distances.append ((train_row, dist))
     distances.sort(key=lambda tup: tup[1])
     neighbors = list()
@@ -49,13 +54,19 @@ def get_neighbors(X_train, user_data, k_value):
     return neighbors
 
 
-#def knn(k, X_train, y, user_data):
-#    distances = []
-#    for i, df_data in enumerate(X_train):
-#        dist = calculate_distance(df_data, user_data)
-#        distances.append({'distance': dist, 'size': y[i]})
-#    distances.sort(key=lambda data: data['distance'])
-#    distances = distances[:k]
+def predict_classification(X_train, test_row, k_value):
+    neighbors = get_neighbors(X_train, test_row, k_value)
+    list_of_predictions = []
+    for size in neighbors:
+        list_of_predictions.append(size[2])
+    prediction = max(set(list_of_predictions), key=list_of_predictions.count)
+    return prediction
+
+
+def k_nearest_nearest_neighbor(X_train, user_data, k_value):
+    for row in user_data:
+        output = predict_classification(X_train, user_data, k_value)
+    return output
 
 
 def main():
@@ -65,43 +76,42 @@ def main():
     # Fething the lists of sizes used later for reference
     sizes = get_sizes(python_data_frame)
 
-    # Define X value
-    df = pd.DataFrame(zip(python_data_frame['height_cm'],python_data_frame['weight_hg']))
-    df.columns = ['height_cm', 'weight_hg']
-
-
-    X_train = list(zip(df.height_cm.tolist(), df.weight_hg.tolist()))
-
     # Define y value
     y = sizes
 
-    # Define k value for number of neighbors
-    k_value = 3
+    # Define X value
+    df = pd.DataFrame(zip(python_data_frame['height_cm'],python_data_frame['weight_hg']))
+    # Adding new column to dataframe
+    df['Size'] = ''
+    # Adding Size list to Size column in dataframe
+    df['Size'] = y
+    df.columns = ['height_cm', 'weight_hg', 'Size']
+    # Creating a list of tuples with height, weight and Size
+    X_train = list(zip(df.height_cm.tolist(), df.weight_hg.tolist(), df.Size.tolist()))
 
-    # User input to array
+
+
+    # User input to list
+    print('Enter height in cm below: ')
     user_height = int(input())
+    print ('Enter weight in hg below: ')
     user_weight = int(input())
-    user_data = [user_height,user_weight]
+    user_data = list((user_height,user_weight))
 
-    # Fething the euclidian distance between values in set and user value
-    neighbors = get_neighbors(X_train, user_data, k_value)
-    print(neighbors)
+    print('************************************')
+
+    # Define k value for number of neighbors
+    print(' Enter number of neighbors to compare with: ')
+    k_value = int(input())
 
 
+    print('************************************')
 
+    # Find the nearest neighbor size
+    nearest_neighbor = k_nearest_nearest_neighbor(X_train, user_data, k_value)
 
-    # Input user information
-    #user_height = input('Enter user height in cm : ')
-    #user_weight = input('Enter user weight in kg: ')
-    #user_weight = int(user_weight)
-    #user_weight_hg = (user_weight*10)
-
-    #nearest_neighbor = []
-    #for length, weight in zip(python_data_frame['height_cm'], python_data_frame['weight_hg']):
-    #    delta_y = abs(user_height) - abs (length)
-    #    delta_x = abs(user_weight_hg) - abs (weight)
-    #    hypotenuse = delta_y **2 + delta_x **2
-    #    nearest_neighbor.append(math.sqrt(hypotenuse))
+    # Print the nearest neighbor
+    print('Recommended size for you is: '+ nearest_neighbor)
 
 
 if __name__ == "__main__":
